5a7b27d92d93b1227f0d72e70e5c86b2e8832e91
verster <rrverster31415@gmail.com>
2023-05-24 06:56:38 +0200

    feature: improved loading component

--- src/components/LoadingCard.tsx
+++ src/components/LoadingCard.tsx
@@ -5,12 +5,12 @@ export default function LoadingCard() {
   const cards = new Array(4).fill(0);
 
   return (
-    <ul className='p-4 list-none flex flex-wrap gap-2'>
+    <ul className='w-full grid lg:grid-cols-3 md:grid-cols-1 gap-4 p-12 grid-rows-12'>
       {cards.map((_, index) => (
-        <Card className='p-2 w-1/4' key={index}>
+        <Card className='p-2' key={index}>
           <li>
-            <div className='flex'>
-              <Skeleton shape='circle' size='4rem' className='mr-2'></Skeleton>
+            <div className='grid'>
+              <Skeleton shape='circle' size='6rem' className='m-auto mb-3'></Skeleton>
               <div style={{ flex: '1' }}>
                 <Skeleton width='100%' className='mb-2'></Skeleton>
                 <Skeleton width='75%'></Skeleton>
